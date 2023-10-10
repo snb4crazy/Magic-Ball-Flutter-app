c55f5eea949d40fcd9d3072f37aab37a43f61326
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-10 00:18:25 -0400

    MagicBall AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,14 @@ void main() => runApp(
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Center(child: Text('Ask Me Anything')),
+      ),
+      body: Container(),
+    );
+    ;
   }
 }
